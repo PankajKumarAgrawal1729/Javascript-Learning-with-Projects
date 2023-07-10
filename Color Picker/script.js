946871ad75d8623836ebe89710d3230d70d7a33a
pankja.k <pankaj.k@zohocorp.com>
2023-07-10 10:44:44 +0530

    Reset Button added and some css properties are updated

--- Color Picker/script.js
+++ Color Picker/script.js
@@ -1,8 +1,13 @@
  let body = document.querySelector("body");
  let btns = document.querySelectorAll(".box");
+ let resetBtn = document.getElementById("resetBtn");
 
 btns.forEach((btn)=>{
     btn.addEventListener('click', (ev)=>{
         body.style.backgroundColor = ev.currentTarget.id;
     })
+});
+
+resetBtn.addEventListener("click", () => {
+    body.style.backgroundColor = "white";
 })
